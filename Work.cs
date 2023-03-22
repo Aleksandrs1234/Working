c504a2c5d97b926e5f282286840f8c5f7ccdc484
Aleksandrs1234 <114806211+Aleksandrs1234@users.noreply.github.com>
2023-03-22 09:22:47 +0200

    Update Work.cs

--- Work.cs
+++ Work.cs
@@ -30,20 +30,45 @@ class Program
     }
     static public void CreateGame(string Name)
     {
-      string Path=@"/home/runner/Work/Testdir/1.Game/"+Name+".txt";
-      FileStream fs=File.Create(Path);
-      fs.Close();
-      using(StreamWriter writetext = new StreamWriter(Path)){  
-        writetext.WriteLine("First");
-        writetext.WriteLine("Second");
-        writetext.WriteLine("Third");
-        writetext.WriteLine("Fourth");
-      }
+        string Path = @"/home/runner/Work/Testdir/1.Game/" + Name + ".txt";
+        FileStream fs = File.Create(Path);
+        fs.Close();
+        using (StreamWriter writetext = new StreamWriter(Path))
+        {
+            string text = Console.ReadLine();
+            writetext.WriteLine("Year of release: " + text);
+            text = Console.ReadLine();
+            writetext.WriteLine("Developer of the game: " + text);
+            text = Console.ReadLine();
+            writetext.WriteLine("Publisher of the game: " + text);
+            text = Console.ReadLine();
+            writetext.WriteLine("Price of the game: " + text);
+        }
+    }
+    static public void CreateDeveloper(string Name)
+    {
+        string Path = @"/home/runner/Work/Testdir/2.Developer/" + Name + ".txt";
+        FileStream fs = File.Create(Path);
+        fs.Close();
+        using (StreamWriter writetext = new StreamWriter(Path))
+        {
+            string text = Console.ReadLine();
+            writetext.WriteLine("Year when company was established: "+text);
+            text=Console.ReadLine();
+            writetext.WriteLine("Amount of employees:" +text);
+            text=Console.ReadLine();
+            writetext.WriteLine("Average salary:" +text);
+            text=Console.ReadLine();
+            writetext.WriteLine("Head of company:" + text);
+        }
     }
 
     public static void Main(string[] args)
     {
         Create();
-        CreateGame("Mort the Chicken");
+        //CreateGame("Mort the Chicken");
+        Console.WriteLine("---------");
+        //CreateGame("Cristian game about stealing burger at 4 am in cube dimension");
+        CreateDeveloper("Cristian game company");
     }
 }
